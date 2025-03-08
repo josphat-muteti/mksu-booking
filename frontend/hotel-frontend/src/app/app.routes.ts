f7f149ff502c7d3f03b9d3243c098af5276df48f
josphat-muteti <josphatmuteti9922@gmail.com>
2025-03-08 18:05:01 +0300

    Created hotel frontend angular project

--- frontend/hotel-frontend/src/app/app.routes.ts
+++ frontend/hotel-frontend/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-	{path: '', component: HomeComponent },
-	{path: 'conference', component: ConferenceBookingComponent },
-	{path: 'rooms', component: RoomsBookingComponent },
-	{path: 'about', component: AboutComponent },
-	{path: 'contact', component: ContactComponent },
-];
+export const routes: Routes = [];
