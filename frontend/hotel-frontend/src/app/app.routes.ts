2b092b01f85b15e6596134da1ce4f6b750ffb0b2
josphat-muteti <josphatmuteti9922@gmail.com>
2025-03-08 11:44:15 +0300

    Setting up basic navigation

--- frontend/hotel-frontend/src/app/app.routes.ts
+++ frontend/hotel-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{path: '', component: HomeComponent },
+	{path: 'conference', component: ConferenceBookingComponent },
+	{path: 'rooms', component: RoomsBookingComponent },
+	{path: 'about', component: AboutComponent },
+	{path: 'contact', component: ContactComponent },
+];
