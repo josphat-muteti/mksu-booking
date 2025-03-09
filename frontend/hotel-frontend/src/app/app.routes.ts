1fb59cc8066f77418981c458f946e5ec632241fb
josphat-muteti <josphatmuteti9922@gmail.com>
2025-03-09 15:33:32 +0300

    Added basic pages,angular material for UI component and navigation

--- frontend/hotel-frontend/src/app/app.routes.ts
+++ frontend/hotel-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConferenceBookingComponent } from './conference-booking/conference-booking.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{path: '', component: HomeComponent },
+	{path: 'conference', component: ConferenceBookingComponent },
+	{path: 'rooms', component: RoomsComponent },
+	{path: 'about', component: AboutComponent },
+	{path: 'contact', component: ContactComponent },
+];
---
+++ frontend/hotel-frontend/src/app/conference-booking/conference-booking.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conference-booking',
+  imports: [],
+  templateUrl: './conference-booking.component.html',
+  styleUrl: './conference-booking.component.scss'
+})
+export class ConferenceBookingComponent {
+
+}
