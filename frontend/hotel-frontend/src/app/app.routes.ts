93ce134bb1d9b8d80ddd490d703d6fa255f10e9c
josphat-muteti <josphatmuteti9922@gmail.com>
2025-03-14 17:05:46 +0300

    Updated project files

--- frontend/hotel-frontend/src/app/app.routes.ts
+++ frontend/hotel-frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ConferenceBookingComponent } from './pages/conference-booking/conference-booking.component';
+import { RoomsComponent } from './pages/rooms/rooms.component';
+import { RestaurantComponent } from './pages/restaurant/restaurant.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'conference-booking', component: ConferenceBookingComponent },
+	{ path: 'rooms', component: RoomsComponent },
+	{ path: 'restaurant', component: RestaurantComponent },
+	{ path: 'about', component: AboutComponent },
+	{ path: 'contact', component: ContactComponent },
+];
